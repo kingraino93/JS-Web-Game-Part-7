8f19686cf4a0a1bae8b06a8fe478b6e442a2ed8c
kingraino93 <76224373+kingraino93@users.noreply.github.com>
2022-06-30 20:08:38 -0400

    Update newNonPlayableCharacter.js
    new

--- newNonPlayableCharacter.js
+++ newNonPlayableCharacter.js
@@ -23,9 +23,11 @@ function newNonPlayableCharacter(x, y) {
 
     setInterval(moveCharacter, 1)
 
-    function walkEast() {
+    async function walkEast(time) {
         direction = 'east'
         element.src = `./assets/red-character/east.gif`
+        await sleep(time)
+        stop()
     }
 
     function walkNorth() {
@@ -47,6 +49,13 @@ function newNonPlayableCharacter(x, y) {
         direction = null
         element.src = `./assets/red-character/static.gif`
     }
+
+    function sleep(time){
+        return new Promise(resolve => {
+            setTimeout(resolve, time)
+        })  
+    }
+    
 
     return {
         element: element,
